b0dedb3bcda569310f451791d5773cbecc47c18f
edmar.james <edmarjamesbautista@gmail.com>
2025-04-01 18:11:32 +0800

    feat(api): create new endpoint to get products with stocks lower or equal to five

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -8,3 +10,9 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    @action(detail=False, methods=["GET"], url_path="low-stock")
+    def low_stock(self, request):
+        low_stock_products = Product.objects.filter(quantity__lte=5)
+        serializer = self.get_serializer(low_stock_products, many=True)
+
+        return Response(serializer.data)
