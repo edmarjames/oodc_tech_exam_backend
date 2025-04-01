b3530e6e65804fd02cc5266175dd3ad4a772f60e
edmar.james <edmarjamesbautista@gmail.com>
2025-04-01 18:40:14 +0800

    feat(api): create update_stock endpoint for increasing or decreasing a product stock

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -16,3 +17,31 @@ class ProductViewSet(ModelViewSet):
         serializer = self.get_serializer(low_stock_products, many=True)
 
         return Response(serializer.data)
+
+    @action(detail=True, methods=["PATCH"], url_path="update-stock")
+    def update_stock(self, request, pk=None):
+        product = self.get_object()
+        amount = request.data.get("amount", 0)
+        action = request.query_params.get("action", None)
+
+        if action.lower() not in ["increase", "decrease"]:
+            return Response({
+                "message": "Invalid action specified. Use 'increase' or 'decrease'."
+            })
+
+        if action.lower() == "increase":
+            response = product.increase_stock(amount)
+        if action.lower() == "decrease":
+            response = product.reduce_stock(amount)
+
+        if response["success"]:
+            return Response({
+                "message": response["message"],
+                "data": ProductSerializer(product).data
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({
+                "message": f"Failed to {action} stock",
+                "error": response["error"]
+            }, status=status.HTTP_400_BAD_REQUEST)
+
