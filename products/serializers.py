4a3e82ce25424652db8cb428a0cc7f86fdb462e8
edmar.james <edmarjamesbautista@gmail.com>
2025-04-01 17:56:32 +0800

    feat(api): modify ProductSerializer to include in_stock field

--- products/serializers.py
+++ products/serializers.py
@@ -3,7 +3,11 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    in_stock = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = "__all__"
+
+    def get_in_stock(self, obj):
+        return obj.is_in_stock()
